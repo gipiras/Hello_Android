6cdecef3ecf768e3348cfe50db95e3290682f552
gipiras <gianlucapiras1@googlemail.com>
2016-02-17 16:15:01 +0100

    Initial commit

---
+++ app/src/main/java/com/hallo/gian_lucapiras/hello_android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hallo.gian_lucapiras.hello_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
