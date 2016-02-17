2f001b88349fe15ca5184d983665cb5d7a4f5e2e
gipiras <gianlucapiras1@googlemail.com>
2016-02-17 16:35:37 +0100

    First Version with welcome message and button description

--- app/src/main/java/com/hallo/gian_lucapiras/hello_android/MainActivity.java
+++ app/src/main/java/com/hallo/gian_lucapiras/hello_android/MainActivity.java
@@ -2,12 +2,23 @@ package com.hallo.gian_lucapiras.hello_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView nachricht;
+    private Button weiter_fertig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nachricht = (TextView) findViewById(R.id.nachricht);
+        weiter_fertig = (Button) findViewById(R.id.weiter_fertig);
+
+        nachricht.setText(R.string.willkommen);
+        weiter_fertig.setText(R.string.weiter);
     }
 }
